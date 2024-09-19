d078c209ddf63053c2330f55f803d3a02573efb8
Stardust <unite0018@gmail.com>
2024-09-20 03:09:31 +0900

    최대 주파수 테스트
    현재 최대 주파수 3M~4MHz 사이임. 데이터시트를 보면 이론상 최대 주파수는 10MHz임. cs 신호와 관련된 타이밍 문제가 있는 것으로 보임. 수정 예정

--- SPI/SPI/spi.v
+++ SPI/SPI/spi.v
@@ -31,7 +31,7 @@ module spi(
     output reg valid
     );
     
-    parameter SCL_FREQ = 1_000_000; // Maximum frequency of SCL: 6.67MHz
+    parameter SCL_FREQ = 3_000_000; // Maximum frequency of SCL: 6.67MHz
     parameter integer SCL_PSC = `CLK_FREQ / SCL_FREQ;   // Prescaler for clock divider
     parameter integer WAIT_FOR_VALID = SCL_PSC * 0.9;
     
