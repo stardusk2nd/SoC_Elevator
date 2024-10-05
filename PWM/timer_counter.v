396209e4bc730a427d2a04d9e3df1e330fcb2eb7
Stardust <unite0018@gmail.com>
2024-10-05 14:22:13 +0900

    예약어 관련 오류 수정

--- PWM/timer_counter.v
+++ PWM/timer_counter.v
@@ -23,9 +23,16 @@
 module timer_counter(
     input clk, reset,
     input [1:0] control,
+<<<<<<< Updated upstream
     input [15:0] max_count,
     input [15:0] compare,
     output reg signal
+=======
+    input [31:0] prescalor,
+    input [31:0] max_count,
+    input [31:0] compare,
+    output reg timer_int, pwm
+>>>>>>> Stashed changes
     );
     
     wire [1:0] mode = control[1:0];
@@ -35,17 +42,28 @@ module timer_counter(
     always @(posedge clk, posedge reset) begin
         if(reset) begin
             count = 0;
+<<<<<<< Updated upstream
             signal = 0;
+=======
+            timer_int = 0;
+            pwm = 0;
+>>>>>>> Stashed changes
             prev_mode = 0;
         end
         else begin
             if(prev_mode != mode) begin
                 count = 0;
+<<<<<<< Updated upstream
                 signal = 0;
+=======
+                timer_int = 0;
+                pwm = 0;
+>>>>>>> Stashed changes
                 prev_mode = mode;
             end
-            // int
+            // timer_int
             if(mode == 2'b01) begin
+<<<<<<< Updated upstream
                 if(count < max_count) begin
                     count = count + 1;
                     signal = 0;
@@ -55,6 +73,23 @@ module timer_counter(
                     signal = 1;
                 end
             end
+=======
+                if(cnt_pres < prescalor)
+                    cnt_pres = cnt_pres + 1;     
+                else begin
+                    cnt_pres = 0;
+                    if(count < max_count) begin
+                        count = count + 1;
+                        timer_int = 0;
+                    end
+                    else begin
+                        count = 0;
+                        timer_int = 1;
+                    end
+                end
+            end
+            // pwm          
+>>>>>>> Stashed changes
             else if(mode == 2'b10) begin
                 if(count < max_count)
                     count = count + 1;
