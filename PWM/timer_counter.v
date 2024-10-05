434742ec2ceb9a301b9c15c072d01aca8743b324
Stardust <unite0018@gmail.com>
2024-10-05 14:27:25 +0900

    오류 수정

--- PWM/timer_counter.v
+++ PWM/timer_counter.v
@@ -23,16 +23,10 @@
 module timer_counter(
     input clk, reset,
     input [1:0] control,
-<<<<<<< Updated upstream
-    input [15:0] max_count,
-    input [15:0] compare,
-    output reg signal
-=======
     input [31:0] prescalor,
     input [31:0] max_count,
     input [31:0] compare,
     output reg timer_int, pwm
->>>>>>> Stashed changes
     );
     
     wire [1:0] mode = control[1:0];
@@ -42,38 +36,19 @@ module timer_counter(
     always @(posedge clk, posedge reset) begin
         if(reset) begin
             count = 0;
-<<<<<<< Updated upstream
-            signal = 0;
-=======
             timer_int = 0;
             pwm = 0;
->>>>>>> Stashed changes
             prev_mode = 0;
         end
         else begin
             if(prev_mode != mode) begin
                 count = 0;
-<<<<<<< Updated upstream
-                signal = 0;
-=======
                 timer_int = 0;
                 pwm = 0;
->>>>>>> Stashed changes
                 prev_mode = mode;
             end
             // timer_int
             if(mode == 2'b01) begin
-<<<<<<< Updated upstream
-                if(count < max_count) begin
-                    count = count + 1;
-                    signal = 0;
-                end
-                else begin
-                    count = 0;
-                    signal = 1;
-                end
-            end
-=======
                 if(cnt_pres < prescalor)
                     cnt_pres = cnt_pres + 1;     
                 else begin
@@ -89,16 +64,19 @@ module timer_counter(
                 end
             end
             // pwm          
->>>>>>> Stashed changes
             else if(mode == 2'b10) begin
-                if(count < max_count)
-                    count = count + 1;
-                else
-                    count = 0;
-                if(count < compare)
-                    signal = 1;
-                else
-                    signal = 0;
+                if(cnt_pres)
+                    cnt_pres = cnt_pres + 1;
+                else begin
+                    if(count < max_count)
+                        count = count + 1;
+                    else
+                        count = 0;
+                    if(count < compare)
+                        pwm = 1;
+                    else
+                        pwm = 0;
+                end
             end
         end
     end
